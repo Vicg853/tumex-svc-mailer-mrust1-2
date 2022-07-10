ec84274a7d0d12016e9e6dc0ac8c4fe0e0421aba
Vicg853 <vicg853@gmail.com>
2022-07-10 02:45:20 -0300

    Regex email validation typo fix

--- src/app_routes.rs
+++ src/app_routes.rs
@@ -4,6 +4,9 @@ use rocket::{
 };
 use rocket::serde::{Deserialize, json::{Json, serde_json}};
 use regex::Regex;
+use rocket_db_pools::{Connection, mongodb};
+
+use crate::MessageCmsDb;
 
 #[derive(Deserialize)]
 pub struct Message {
@@ -21,9 +24,9 @@ struct ValidError<'c> {
 
 impl<'c> Message {
     fn is_valid(&self) -> Result<(), ValidError<'c>> {
-        let email_rgx = Regex::new("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$").unwrap();
+        const EMAIL_RGX: &str = r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9]+(\.[a-zA-Z0-9-]{0,61})+$";
 
-        if !email_rgx.is_match(&self.from) {
+        if Regex::new(EMAIL_RGX).unwrap().is_match(&self.from) {
             Ok(())
         } else {
             Err(ValidError {
@@ -42,19 +45,20 @@ impl<'c> Message {
 pub async fn send_message(message: Json<Message>) -> status::Custom<content::RawJson<String>> {
     let message = message.into_inner();
     let validated = message.is_valid();
-    
-    match validated {
-        Err(e) => {
-            let json_response = serde_json::json!({
-                "message": e.message
-            });
 
-            status::Custom(
-            HttpStatus::new(e.code), 
+    if validated.is_err() {
+        let valid_err = validated.unwrap_err();
+        
+        let json_response = serde_json::json!({
+            "message": valid_err.message 
+        });
+    
+        return status::Custom(
+            HttpStatus::new(valid_err.code), 
             content::RawJson(json_response.to_string()))
-        },
-        Ok(_) => status::Custom(
-            HttpStatus::new(200), 
-            content::RawJson("Message sent".to_string()))
     }
+    
+    status::Custom(
+        HttpStatus::new(200), 
+        content::RawJson("Message sent".to_string()))
 }
