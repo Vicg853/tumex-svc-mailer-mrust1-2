a063d95abc927e37766a7e0d994ae1696a04c004
Vicg853 <vicg853@gmail.com>
2022-07-09 23:27:26 -0300

    Mongo DB config

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 #[macro_use] extern crate rocket;
 
+use rocket_db_pools::Database;
+use rocket_db_pools::mongodb;
+
 mod app_routes;
+
+//Opening connection to CMS MongoDb database
+#[derive(Database)]
+#[database("tux_cms_db")]
+struct MessageCmsDb(mongodb::Client);
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .attach(MessageCmsDb::init())
         .mount("/", routes![app_routes::send_message])
 }
