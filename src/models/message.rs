f71f3add53afcc16f997af9683ef8c80dd6b73bf
Vicg853 <vicg853@gmail.com>
2022-07-10 19:25:53 -0300

    Saving current progress

--- src/models/message.rs
+++ src/models/message.rs
@@ -1,1 +1,18 @@
-use mongodb::{bson::doc, options::ClientOptions, Client};
+use serde::{Deserialize, Serialize};
+use mongodb::bson::{
+   oid::{ObjectId}, 
+   DateTime
+};
+
+#[derive(Serialize, Deserialize)]
+pub struct Message {
+   #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+   pub id: Option<ObjectId>,
+   #[serde(rename = "createdAt", skip_serializing_if = "Option::is_none")]
+   pub created_at: Option<DateTime>,
+   pub from: String,
+   pub name: String,
+   pub subject: String,
+   pub message: String,
+   pub read: bool
+}
---
+++ src/mongo.rs
@@ -0,0 +1,53 @@
+use std::{env, panic};
+use mongodb::{
+   options::ClientOptions,
+   Collection,
+   Client,
+   self
+};
+
+use crate::models::message::Message;
+
+struct MessageCmsDb {
+   client: Client,
+   msg_col: Collection<Message>
+}
+
+impl MessageCmsDb {
+   pub async fn init() -> Self {
+      let CMS_DB_CLUST_URI = match env::var("CMS_DB_CLUST_URI") {
+         Ok(val) => val,
+         Err(_) => panic!("CMS_DB_CLUST_URI environment must be set")
+      };
+
+      let CMS_MSG_DB_NAME = match env::var("CMS_MSG_DB_NAME") {
+         Ok(val) => val,
+         Err(_) => panic!("CMS_MSG_DB_NAME environment must be set")
+      };
+
+      //TODO - finish tls setup
+      //let tls_opts = mongodb::options::TlsOptions {
+      //   ..Default::default()
+      //};
+
+      let client_opts = ClientOptions::parse(CMS_DB_CLUST_URI)
+         .await.expect("Failed to parse mongodb CMS DB's connection URI");
+
+      match Client::with_options(client_opts) {
+         Ok(client) => {
+            let msg_col = client.database(CMS_MSG_DB_NAME.as_str())
+            .collection("messages");
+
+            MessageCmsDb {
+               client,
+               msg_col
+            }
+         },
+         Err(err) => panic!("Failed to connect to CMS DB Cluster: {}", err)
+      }
+   }
+
+   pub fn get_client<'a>(&'a mut self) -> &'a mut Client {
+      &mut self.client
+   }
+}
