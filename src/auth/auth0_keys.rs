e595961c70a08addea5237765d51149534c388c5
Vicg853 <vicg853@gmail.com>
2022-07-16 23:25:03 -0300

    Many fixes with auth0_jwt key set methods and structs

--- src/auth/auth0_keys.rs
+++ src/auth/auth0_keys.rs
@@ -3,7 +3,8 @@ use std::{vec::Vec, env};
 use reqwest::{get, Error as ReqwestErr};
 use openssl::{
    error::ErrorStack as RsaErrStack, 
-   rsa::Rsa
+   rsa::Rsa,
+   bn::BigNum
 };
 
 fn tennant_endpoint() -> String {
@@ -15,6 +16,9 @@ fn tennant_endpoint() -> String {
 const PUB_KEYS_PATH: &str = "/.well-known/jwks.json";
 
 mod auth0_jwk_set {
+    use serde::Deserialize;
+
+    #[derive(Deserialize)]
    pub struct TenantKey {
       pub alg: String,
       pub kty: String,
@@ -25,16 +29,21 @@ mod auth0_jwk_set {
       pub kid: String,
       pub x5t: String
    }
+
+   #[derive(Deserialize)]
    pub struct TenantKeysResponse {
       pub keys: Vec<TenantKey>
    }
 }
 
+#[derive(Debug)]
 struct KeyComponents {
    pub modulus: String,
    pub exponent: String,
    pub kid: String,
 }
+
+#[derive(Debug)]
 pub struct PublicKeys(Vec<KeyComponents>);
 
 async fn fetch_components() -> Result<Vec<KeyComponents>, ReqwestErr> {
@@ -52,12 +61,12 @@ async fn fetch_components() -> Result<Vec<KeyComponents>, ReqwestErr> {
             ::<auth0_jwk_set::TenantKeysResponse>().await;
 
          if json.is_err() {
-            warn!("Failed to parse public JWT key set from auth0. The following error was encountered: {}", json.err().unwrap());
-            Err(json.err().unwrap())
+            warn!("Failed to parse public JWT key set from auth0. The following error was encountered: {}", json.as_ref().err().unwrap());
+            return Err(json.err().unwrap());
          } 
 
          let json = json.unwrap();
-         let keys_vec = Vec::new();
+         let mut keys_vec: Vec<KeyComponents> = Vec::new();
          for key in json.keys.into_iter() {
             keys_vec.push(KeyComponents {
                modulus: key.n,
@@ -76,8 +85,8 @@ impl PublicKeys {
       let keys = fetch_components().await;
 
       if keys.is_err() {
-         warn!("Failed to fetch public JWT key set components from auth0. The following error was encountered: {}", keys.err().unwrap());
-         Err(keys.err().unwrap())
+         warn!("Failed to fetch public JWT key set components from auth0. The following error was encountered: {:?}", keys.as_ref().err().unwrap());
+         return Err(keys.err().unwrap())
       }
 
       Ok(PublicKeys(keys.unwrap()))
@@ -87,8 +96,8 @@ impl PublicKeys {
       let keys = fetch_components().await;
 
       if keys.is_err() {
-         warn!("Failed to fetch public JWT key set components from auth0. The following error was encountered: {}", keys.err().unwrap());
-         Err(keys.err().unwrap())
+         warn!("Failed to fetch public JWT key set components from auth0. The following error was encountered: {:?}", keys.as_ref().err().unwrap());
+         return Err(keys.err().unwrap())
       }
 
       self.0 = keys.unwrap();
@@ -96,26 +105,28 @@ impl PublicKeys {
    }
 
    pub fn get_components(&self, kid: &str) -> Option<&KeyComponents> {
-      self.0.iter().find(|key| {
-         **key.kid == kid
+      self.0.iter().find(|&key| {
+         *key.kid == *kid
       })
    }
 
-   pub fn rsa_from_components(&self, kid: &str) -> Option<Result<String, RsaErrStack>> {
+   pub fn rsa_from_components(&self, kid: &str) -> Option<Result<Vec<u8>, RsaErrStack>> {
       let components = self.get_components(kid)?;
 
-      match Rsa::from_public_components(
-         components.modulus.parse::<u64>().unwrap(),
-         components.exponent.parse::<u64>().unwrap()
-      ) {
+      let modulus = BigNum::from_dec_str(&components.modulus)
+         .ok()?;
+      let exponent = BigNum::from_dec_str(&components.exponent)
+         .ok()?;
+
+      match Rsa::from_public_components(modulus, exponent) {
          Ok(rsa) => {
             let pem = rsa.public_key_to_pem_pkcs1();
 
             if pem.is_err() {
-               warn!("Failed to convert public key to PEM format. The following error was encountered: {}", pem.err().unwrap());
-               Some(Err(pem.err()))
+               warn!("Failed to convert public key to PEM format. The following error was encountered: {}", pem.as_ref().err().unwrap());
+               Some(Err(pem.err().unwrap()))
             } else {
-               Some(pem.unwrap())
+               Some(Ok(pem.unwrap()))
             }
          }
          Err(err) => {
@@ -123,7 +134,5 @@ impl PublicKeys {
             Some(Err(err))
          }
       }
-
-
    }
 }
