59a7029b7a70bb2d2a3777d42cbf6ddc10b76207
Vicg853 <vicg853@gmail.com>
2022-07-23 17:30:42 -0300

    Adding token claims struct and moving some module positions

--- src/auth/auth0_claims.rs
+++ src/auth/auth0_claims.rs
@@ -1,43 +1,62 @@
-pub enum IsClaims {
-   TUMEX,
-   FRIENDS_NORMAL,
-   FRIENDS_CLOSE,
-   FRIENDS_BFF,
-   FAMILY_FIRST,
-   FAMILY_SECOND,
-   FAMILY_THIRD,
-   SUDO_LOW,
-   SUDO_HIGH,
-}
-
-impl IsClaims {
-   pub fn as_str(&self) -> &'static str {
-      match self {
-         IsClaims::TUMEX => "is:tumex",
-         IsClaims::FRIENDS_NORMAL => "is:friends:normal",
-         IsClaims::FRIENDS_CLOSE => "is:friends:close",
-         IsClaims::FRIENDS_BFF => "is:friends:bff",
-         IsClaims::FAMILY_FIRST => "is:family:first-deg",
-         IsClaims::FAMILY_SECOND => "is:family:second-deg",
-         IsClaims::FAMILY_THIRD => "is:family:third-deg",
-         IsClaims::SUDO_LOW => "is:sudo:low",
-         IsClaims::SUDO_HIGH => "is:sudo:high"
+pub mod Auth0Perms {
+   pub enum IsClaims {
+      TUMEX,
+      FRIENDS_NORMAL,
+      FRIENDS_CLOSE,
+      FRIENDS_BFF,
+      FAMILY_FIRST,
+      FAMILY_SECOND,
+      FAMILY_THIRD,
+      SUDO_LOW,
+      SUDO_HIGH,
+   }
+   
+   impl IsClaims {
+      pub fn as_str(&self) -> &'static str {
+         match self {
+            IsClaims::TUMEX => "is:tumex",
+            IsClaims::FRIENDS_NORMAL => "is:friends:normal",
+            IsClaims::FRIENDS_CLOSE => "is:friends:close",
+            IsClaims::FRIENDS_BFF => "is:friends:bff",
+            IsClaims::FAMILY_FIRST => "is:family:first-deg",
+            IsClaims::FAMILY_SECOND => "is:family:second-deg",
+            IsClaims::FAMILY_THIRD => "is:family:third-deg",
+            IsClaims::SUDO_LOW => "is:sudo:low",
+            IsClaims::SUDO_HIGH => "is:sudo:high"
+         }
+      }
+   }
+   
+   pub enum Permissions {
+      MAILER_BASE_ACCESS,
+      MAILER_WEBP_MSGS_READ,
+      MAILER_WEBP_MSGS_DEL
+   }
+   
+   impl Permissions {
+      pub fn as_str(&self) -> &'static str {
+         match self {
+            Permissions::MAILER_BASE_ACCESS => "mailer:baseaccess",
+            Permissions::MAILER_WEBP_MSGS_READ => "mailer:webp:messages:read",
+            Permissions::MAILER_WEBP_MSGS_DEL => "mailer:webp:messages:delete",
+         }
       }
    }
 }
 
-pub enum Permissions {
-   MAILER_BASE_ACCESS,
-   MAILER_WEBP_MSGS_READ,
-   MAILER_WEBP_MSGS_DEL
-}
 
-impl Permissions {
-   pub fn as_str(&self) -> &'static str {
-      match self {
-         Permissions::MAILER_BASE_ACCESS => "mailer:baseaccess",
-         Permissions::MAILER_WEBP_MSGS_READ => "mailer:webp:messages:read",
-         Permissions::MAILER_WEBP_MSGS_DEL => "mailer:webp:messages:delete",
-      }
+pub mod Auth0TokenRelated {
+   pub struct AudienceIdentifier(String);
+   pub struct AudienceUri(String);
+
+   pub struct Auth0TokenFields {
+      pub iss: String,
+      pub sub: String,
+      pub aud: Option<(AudienceIdentifier, AudienceUri)>,
+      pub azp: String,
+      pub exp: usize,
+      pub iat: usize,
+      pub scope: Vec<String>,
+      pub permissions: Vec<String>,
    }
 }
--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,5 +1,7 @@
 mod auth0_claims;
 mod auth0_keys;
+mod auth0_token_data;
 
 pub use auth0_claims::*;
 pub use auth0_keys::*;
+pub use auth0_token_data::*;
